c7502fdf2c19ac2e7e5055adb067675b4ad94653
Jarsey45 <bartek.yt01@gmail.com>
2024-12-20 16:57:48 +0100

    basic entities and migrations setup

--- app/src/lib/db.ts
+++ app/src/lib/db.ts
@@ -1,7 +1,19 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { Note } from './entities/Note';
-import { User } from './entities/User';
+import { Note } from "../entities/Note";
+import { User } from "../entities/User";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// console.log('Database connection details:', {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   username: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   // Don't log the actual password
+//   hasPassword: !!process.env.DB_PASSWORD
+// });
 
 export const AppDataSource = new DataSource({
 	type: "postgres",
@@ -13,15 +25,17 @@ export const AppDataSource = new DataSource({
 	synchronize: process.env.NODE_ENV === "development",
 	logging: process.env.NODE_ENV === "development",
 	entities: [Note, User],
-	migrations: ["app/lib/migrations/**/*.ts"],
+	migrations: ["app/src/migrations/**/*.ts"],
+	migrationsTableName: "app",
 });
 
 let initialized = false;
 
-export async function initDB() {
+async function initDB() {
 	if (!initialized) {
 		await AppDataSource.initialize();
 		initialized = true;
 	}
 	return AppDataSource;
 }
+
---
+++ app/src/migrations/1733584543528-InitialMigration.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialMigration1733584543528 implements MigrationInterface {
+    name = 'InitialMigration1733584543528'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "role" character varying NOT NULL DEFAULT 'default', CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "note" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "content" text NOT NULL, "color" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "tags" text, "userId" integer, CONSTRAINT "PK_96d0c172a4fba276b1bbed43058" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "user_friends_user" ("userId_1" integer NOT NULL, "userId_2" integer NOT NULL, CONSTRAINT "PK_f2b5631d91f6b7fda632135932f" PRIMARY KEY ("userId_1", "userId_2"))`);
+        await queryRunner.query(`CREATE INDEX "IDX_04840fd160b733de706a336013" ON "user_friends_user" ("userId_1") `);
+        await queryRunner.query(`CREATE INDEX "IDX_e81f236c989f3fd54836b50a12" ON "user_friends_user" ("userId_2") `);
+        await queryRunner.query(`ALTER TABLE "note" ADD CONSTRAINT "FK_5b87d9d19127bd5d92026017a7b" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "user_friends_user" ADD CONSTRAINT "FK_04840fd160b733de706a3360134" FOREIGN KEY ("userId_1") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`ALTER TABLE "user_friends_user" ADD CONSTRAINT "FK_e81f236c989f3fd54836b50a12d" FOREIGN KEY ("userId_2") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user_friends_user" DROP CONSTRAINT "FK_e81f236c989f3fd54836b50a12d"`);
+        await queryRunner.query(`ALTER TABLE "user_friends_user" DROP CONSTRAINT "FK_04840fd160b733de706a3360134"`);
+        await queryRunner.query(`ALTER TABLE "note" DROP CONSTRAINT "FK_5b87d9d19127bd5d92026017a7b"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_e81f236c989f3fd54836b50a12"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_04840fd160b733de706a336013"`);
+        await queryRunner.query(`DROP TABLE "user_friends_user"`);
+        await queryRunner.query(`DROP TABLE "note"`);
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
